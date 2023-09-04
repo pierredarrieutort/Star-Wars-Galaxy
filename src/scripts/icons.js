ba7a7c7cb1aeda03457688954d333d1b0bea6fcd
Pierre Darrieutort <pierre.da@helloasso.com>
2023-09-04 23:15:34 +0200

    reorganize

---
+++ src/scripts/icons.js
@@ -0,0 +1,379 @@
+//Icons
+
+//Invisible Icon
+export const invisible1 = L.icon({
+  iconUrl: require('../images/planet-shadow.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [1, 1], // size of the shadow
+  iconAnchor: [5, 0], // point of the icon which will correspond to marker's location
+  shadowAnchor: [0, 0], // the same for the shadow
+  popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
+})
+
+//Non-Movie Planet Icons
+export const pltIconBlk = L.icon({
+  iconUrl: require('../images/planet-blk.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconCan = L.icon({
+  iconUrl: require('../images/planet-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconLeg = L.icon({
+  iconUrl: require('../images/planet-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconCaL = L.icon({
+  iconUrl: require('../images/planet-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Movie Planet Icons
+export const pltIconBlk2 = L.icon({
+  iconUrl: require('../images/planetMov-blk.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconCan2 = L.icon({
+  iconUrl: require('../images/planetMov-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconLeg2 = L.icon({
+  iconUrl: require('../images/planetMov-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pltIconCaL2 = L.icon({
+  iconUrl: require('../images/planetMov-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Non-Movie Nebula Icons
+
+export const nebIconBlk = L.icon({
+  iconUrl: require('../images/nebula-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [12, 12], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const nebIconCan = L.icon({
+  iconUrl: require('../images/nebula-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const nebIconLeg = L.icon({
+  iconUrl: require('../images/nebula-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const nebIconCaL = L.icon({
+  iconUrl: require('../images/nebula-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [16, 16], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [8, 8],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Movie Nebula Icons
+export const nebIconCan2 = L.icon({
+  iconUrl: require('../images/nebulaMov-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const nebIconLeg2 = L.icon({
+  iconUrl: require('../images/nebulaMov-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const nebIconCaL2 = L.icon({
+  iconUrl: require('../images/nebulaMov-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Non-Movie Station Icons
+export const staIconCan = L.icon({
+  iconUrl: require('../images/staton-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const staIconLeg = L.icon({
+  iconUrl: require('../images/staton-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const staIconCaL = L.icon({
+  iconUrl: require('../images/staton-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Movie Station Icons
+export const staIconBlk2 = L.icon({
+  iconUrl: require('../images/statonMov-blk.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const staIconCan2 = L.icon({
+  iconUrl: require('../images/statonMov-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const staIconLeg2 = L.icon({
+  iconUrl: require('../images/statonMov-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const staIconCaL2 = L.icon({
+  iconUrl: require('../images/statonMov-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [20, 20], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [10, 10],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Non-Movie Phenomena Icons
+
+export const pheIconBlk = L.icon({
+  iconUrl: require('../images/phenom-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pheIconCan = L.icon({
+  iconUrl: require('../images/phenom-grn.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pheIconLeg = L.icon({
+  iconUrl: require('../images/phenom-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const pheIconCaL = L.icon({
+  iconUrl: require('../images/phenom-grn-blu.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+//Others/Old
+export const planetIcon = L.icon({
+  iconUrl: require('../images/planet-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const planetIcon2 = L.icon({
+  iconUrl: require('../images/planet-movie.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [14, 14], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const statonIcon = L.icon({
+  iconUrl: require('../images/staton-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [12, 12], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const statonIcon2 = L.icon({
+  iconUrl: require('../images/staton-movie.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [18, 18], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const clustrIcon = L.icon({
+  iconUrl: require('../images/clustr-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [13, 13], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+export const blkhleIcon = L.icon({
+  iconUrl: require('../images/blkhle-normal.png'),
+  shadowUrl: require('../images/planet-shadow.png'),
+  iconSize: [10, 10], // size of the icon
+  shadowSize: [10, 10], // size of the shadow
+  iconAnchor: [5, 5],   // point of the icon which will correspond to marker's location
+  shadowAnchor: [5, 5],   // the same for the shadow
+  popupAnchor: [-5, -20] // point from which the popup should open relative to the iconAnchor
+})
+
+
+export default {
+  invisible1,
+  pltIconBlk,
+  pltIconCan,
+  pltIconLeg,
+  pltIconCaL,
+  pltIconBlk2,
+  pltIconCan2,
+  pltIconLeg2,
+  pltIconCaL2,
+  nebIconBlk,
+  nebIconCan,
+  nebIconLeg,
+  nebIconCaL,
+  nebIconCan2,
+  nebIconLeg2,
+  nebIconCaL2,
+  staIconCan,
+  staIconLeg,
+  staIconCaL,
+  staIconBlk2,
+  staIconCan2,
+  staIconLeg2,
+  staIconCaL2,
+  pheIconBlk,
+  pheIconCan,
+  pheIconLeg,
+  pheIconCaL,
+  planetIcon,
+  planetIcon2,
+  statonIcon,
+  statonIcon2,
+  clustrIcon,
+  blkhleIcon
+}
