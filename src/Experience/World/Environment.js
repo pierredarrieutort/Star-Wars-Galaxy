2dfc1ab0c6da5e03d5eda764445a8e5da601ac7e
Pierre Darrieutort <pierre.da@helloasso.com>
2023-10-05 14:24:38 +0200

    rename Env debug folder

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -10,7 +10,7 @@ export default class Environment {
 
         // Debug
         if (this.debug.active) {
-            this.debugFolder = this.debug.ui.addFolder('environment')
+            this.debugFolder = this.debug.ui.addFolder('Environment')
 
             this.sunLightHelper = null
             this.isDisplayedSunLightHelper = true
